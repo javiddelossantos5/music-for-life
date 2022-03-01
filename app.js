741197e8d79eb2a35b3d9171d7a8e4253b9403c1
Javid De Los Santos <javiddelossantos5@gmail.com>
2022-03-01 14:34:41 -0700

    added donation page

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
+//NavBar Start
+
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
 toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active')
 })
+
+//NavBar End
+
